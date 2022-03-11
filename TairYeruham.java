cbb3b8014b851a5a199c368e065a7dce2d88645d
Tair-Yeruham <tairyeruham@gmail.com>
2022-03-11 18:03:54 +0200

    change print

--- TairYeruham.java
+++ TairYeruham.java
@@ -1,7 +1,7 @@
 public class TairYeruham{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Tair Yeruham");
+			System.out.println("Tair Yeruham change print :)");
 		}
 	}
 }
